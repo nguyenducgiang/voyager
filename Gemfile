6afc111eb27d6576ab2fb117a490d84b4233c294
Giang Nguyen <giangnguyen1089@gmail.com>
2024-07-15 14:32:44 +0700

    add User model and authentication

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.2.3"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
 gem "bootsnap", require: false
+gem "devise"
 gem "haml-rails"
 gem "importmap-rails"
 gem "jbuilder"
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\z/ }
+end
